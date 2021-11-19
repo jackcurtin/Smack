f37d20605a1f1536d4e2b6d0da86e10b16fad799
Jack <jackcurtin94@gmail.com>
2021-11-19 13:47:29 -0600

    created loginActivity

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.smack
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -47,6 +46,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked (view: View){
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
 
     }
 

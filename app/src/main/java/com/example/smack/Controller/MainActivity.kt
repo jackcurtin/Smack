1cb818ea60250a28d1e0e9296018400cc367bfbb
Jack <jackcurtin94@gmail.com>
2021-11-23 11:01:25 -0600

    made login nav bar dynamic based on login status

--- app/src/main/java/com/example/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/smack/Controller/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.smack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,13 +18,22 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.smack.R
+import com.example.smack.Services.AuthService
+import com.example.smack.Services.UserDataService
+import com.example.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import com.example.smack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var userNameNavHeader: TextView
+    lateinit var userEmailNavHeader: TextView
+    lateinit var userImageNavHeader: ImageView
+    lateinit var loginBtnNavHeader: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +42,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
+
+        userNameNavHeader = findViewById(R.id.userNameNavHeader)
+        userEmailNavHeader = findViewById(R.id.userEmailNavHeader)
+        userImageNavHeader = findViewById(R.id.userImageNavHeader)
+        loginBtnNavHeader = findViewById(R.id.loginBtnNavHeader)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
@@ -39,6 +60,22 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
+            IntentFilter(BROADCAST_USER_DATA_CHANGE))
+    }
+
+    private val userDataChangeReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                loginBtnNavHeader.text = "Logout"
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -47,8 +84,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked (view: View){
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+        if (AuthService.isLoggedIn){
+            UserDataService.logout()
+            userNameNavHeader.text = "Login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "Login"
+        } else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
 
     }
 

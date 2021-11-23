9fbad6795486ddf3522aafc03b9d8966e797ba1d
Jack <jackcurtin94@gmail.com>
2021-11-23 11:44:34 -0600

    created navigation for successful login attempt

--- app/src/main/java/com/example/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/smack/Controller/LoginActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import com.example.smack.R
+import com.example.smack.Services.AuthService
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var loginEmailTxt: TextView
+    lateinit var loginPasswordTxt: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loginEmailTxt = findViewById(R.id.loginEmailTxt)
+        loginPasswordTxt = findViewById(R.id.loginPasswordTxt)
         setContentView(R.layout.activity_login)
     }
 
@@ -20,6 +27,17 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View){
+        val email = loginEmailTxt.text.toString()
+        val password = loginPasswordTxt.text.toString()
 
+        AuthService.loginUser(this, email, password) { loginSuccess ->
+            if(loginSuccess) {
+                AuthService.findUserByEmail(this) { findSuccess ->
+                    if(findSuccess){
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }

999911bc7234fd37045e4b7c68809b0fbb89d560
Jack <jackcurtin94@gmail.com>
2021-11-29 11:20:56 -0600

    debugged jsonObject request

--- app/src/main/java/com/example/smack/Services/MessageService.kt
+++ app/src/main/java/com/example/smack/Services/MessageService.kt
@@ -3,6 +3,7 @@ package com.example.smack.Services
 import android.content.Context
 import android.util.Log
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.smack.Model.Channel
@@ -14,17 +15,17 @@ object MessageService {
     val channels = ArrayList<Channel>()
 
     fun getChannels(context: Context, complete: (Boolean) -> Unit) {
-        val channelsRequest = object : JsonObjectRequest(Method.GET, URL_GET_CHANNELS, null, Response.Listener { response ->
+        val channelsRequest = object : JsonArrayRequest(Method.GET, URL_GET_CHANNELS, null, Response.Listener { response ->
             try {
                 for (x in 0 until response.length()) {
-                    println(response)
-//                    val channel = response.getJSONObject()
-//                    val name = channel.getString("name")
-//                    val chanDesc = channel.getString("description")
-//                    val channelId = channel.getString("_id")
-//
-//                    val newChannel = Channel(name, chanDesc, channelId)
-//                    this.channels.add(newChannel)
+//                    println(x.toString())
+                    val channel = response.getJSONObject(x)
+                    val name = channel.getString("name")
+                    val chanDesc = channel.getString("description")
+                    val channelId = channel.getString("_id")
+
+                    val newChannel = Channel(name, chanDesc, channelId)
+                    this.channels.add(newChannel)
                 }
                 complete(true)
 
